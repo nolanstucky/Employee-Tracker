54fe4735bad742c9aff8b336465094dad3282a9d
Nolan Stucky <nolan@stucky.net>
2021-02-28 16:59:04 -0800

    setup initial inquirer prompt

--- app.js
+++ app.js
@@ -5,9 +5,43 @@ const consoleTable = require("console.table");
 //connection to mysql database
 const connection = mysql.createConnection({
     host: "localhost",
-    port: 8080,
+    port: 3306,
     user: "root",
     password: "password",
     database: "employeeTrackerDB"
 });
 
+connection.connect(function(err){
+    if(err) throw err
+    console.log("Connected as Id" + connection.threadId)
+    init();
+})
+
+
+function init() {
+    inquirer.prompt([
+        {
+            type: "list",
+            name: "choice",
+            message: "What would you like to do?",
+            choices: [
+                "View All Employees",
+                "View All Employees By Department",
+                "View All Employees By Role",
+                "Add Employee",
+                "Update Employee Role",
+                "Add Department",
+                "Add Roles"
+            ]
+
+        }
+    ])
+    .then(answers =>{
+        switch (answers.choice) {
+            case "View All Employees":
+                console.log("chris is an idiot")
+            break;
+
+        }
+    })
+}
